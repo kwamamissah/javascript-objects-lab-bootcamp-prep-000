f143aac1e5243453974fc03243d9a90e37ab4694
kwamamissah@gmail.com <kwamamissah@gmail.com>
2018-09-09 19:15:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,5 +26,7 @@ function deleteFromObjectByKey(object, key){
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
 }
